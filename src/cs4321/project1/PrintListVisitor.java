a92b37bd15dcd427c28b03b602b5c5913b3b8de7
gkeswani92 <gauravkeswani92@gmail.com>
2015-09-01 19:49:38 -0400

    Refactored Print List Visitor & Java Docs
    Added Java Docs for all the methods and refactored the checking and
    recursive calling of the node into a single method.
    
    NOTE: Verify working of the UnaryMinusListNode

--- src/cs4321/project1/PrintListVisitor.java
+++ src/cs4321/project1/PrintListVisitor.java
@@ -13,15 +13,29 @@ public class PrintListVisitor implements ListVisitor {
     private String result; // running string representation
 
 	public PrintListVisitor() {
-		// TODO fill me in
-        //result = "";
+        result = "";
 	}
 
+	/**
+     * Getter method to get the final pretty printed list
+     */
 	public String getResult() {
-		// TODO fill me in
-		//return result;
+		return result.trim();
 	}
-
+	
+	/**
+	 * Method to check if a node has a next node and recursively calls 
+	 * the same if true
+	 * 
+	 * @param node
+	 * 				the node who's child we need to test for
+	 */				
+	public void checkAndCallNextNode(ListNode node){
+		if (node.getNext() != null) {
+        	node.getNext().accept(this);
+        }
+	}
+	
     /**
      * Visit method for leaf node; just concatenates the numeric value to the
      * running string
@@ -31,37 +45,84 @@ public class PrintListVisitor implements ListVisitor {
      */
 	@Override
 	public void visit(NumberListNode node) {
-		// TODO fill me in
-        //result += node.getData();
-
+        result += node.getData(); //Concatenates the data from the number node to the result
+        if (node.getNext() != null) {
+        	result += " ";
+        	node.getNext().accept(this);
+        }
 	}
 
+	/**
+	 * Visit method for the addition list node; concatenates a + sign
+	 * to the running string and recursively calls the next node
+	 * using visitor patter
+	 * 
+	 * @param node
+	 * 				the node to be visited
+	 */
 	@Override
 	public void visit(AdditionListNode node) {
-		// TODO fill me in
-
+		result += "+ ";
+		checkAndCallNextNode(node);
+		result += " ";
 	}
 
+	/**
+	 * Visit method for the subraction list node; concatenates a - sign
+	 * to the running string and recursively calls the next node
+	 * using visitor patter
+	 * 
+	 * @param node
+	 * 				the node to be visited
+	 */
 	@Override
 	public void visit(SubtractionListNode node) {
-		// TODO fill me in
+		result += "- ";
+		checkAndCallNextNode(node);
+		result += " ";
 	}
 
+	/**
+	 * Visit method for the multiplication list node; concatenates a * sign
+	 * to the running string and recursively calls the next node
+	 * using visitor patter
+	 * 
+	 * @param node
+	 * 				the node to be visited
+	 */
 	@Override
 	public void visit(MultiplicationListNode node) {
-		// TODO fill me in
-
+		result += "* ";
+		checkAndCallNextNode(node);
+		result += " ";
 	}
 
+	/**
+	 * Visit method for the division list node; concatenates a + sign
+	 * to the running string and recursively calls the next node
+	 * using visitor patter
+	 * 
+	 * @param node
+	 * 				the node to be visited
+	 */
 	@Override
 	public void visit(DivisionListNode node) {
-		// TODO fill me in
-
+		result += "/ ";
+		checkAndCallNextNode(node);
+		result += " ";
 	}
 
+	/**
+	 * Visit method for the unary minus list node; concatenates a ~ sign
+	 * to the running string and recursively calls the next node
+	 * using visitor patter
+	 * 
+	 * @param node
+	 * 				the node to be visited
+	 */
 	@Override
 	public void visit(UnaryMinusListNode node) {
-		// TODO fill me in
+		result += "~ ";
+		checkAndCallNextNode(node);
 	}
-
 }
