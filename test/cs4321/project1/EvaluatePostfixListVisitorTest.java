1f57f1b70e50df23ca43140c40462a8096aed986
gkeswani92 <gauravkeswani92@gmail.com>
2015-09-06 12:53:21 -0400

    Postfix List Evaluation Test Cases

--- test/cs4321/project1/EvaluatePostfixListVisitorTest.java
+++ test/cs4321/project1/EvaluatePostfixListVisitorTest.java
@@ -53,5 +53,79 @@ public class EvaluatePostfixListVisitorTest {
 		n1.accept(v1);
 		assertEquals(6.0, v1.getResult(), DELTA);
 	}
-
+	
+	@Test
+	public void testAdditionMultiplicationDivision(){
+		ListNode n1 = new NumberListNode(17.0);
+		ListNode n2 = new NumberListNode(10.0);
+		ListNode n3 = new AdditionListNode();
+		ListNode n4 = new NumberListNode(3.0);
+		ListNode n5 = new MultiplicationListNode();
+		ListNode n6 = new NumberListNode(9.0);
+		ListNode n7 = new DivisionListNode();
+		n1.setNext(n2);
+		n2.setNext(n3);
+		n3.setNext(n4);
+		n4.setNext(n5);
+		n5.setNext(n6);
+		n6.setNext(n7);
+		EvaluatePostfixListVisitor v1 = new EvaluatePostfixListVisitor();
+		n1.accept(v1);
+		assertEquals(9.0, v1.getResult(), DELTA);
+	}
+	
+	@Test
+	public void testComplexAdditionSubractionMultiplication() {
+		ListNode n1 = new NumberListNode(4.0);
+		ListNode n2 = new NumberListNode(2.0);
+		ListNode n3 = new AdditionListNode();
+		ListNode n4 = new NumberListNode(3.0);
+		ListNode n5 = new NumberListNode(5.0);
+		ListNode n6 = new NumberListNode(1.0);
+		ListNode n7 = new SubtractionListNode();
+		ListNode n8 = new MultiplicationListNode();
+		ListNode n9 = new AdditionListNode();
+		n1.setNext(n2);
+		n2.setNext(n3);
+		n3.setNext(n4);
+		n4.setNext(n5);
+		n5.setNext(n6);
+		n6.setNext(n7);
+		n7.setNext(n8);
+		n8.setNext(n9); //Expression is 4 2 + 3 5 1 -  * + 
+		
+		EvaluatePostfixListVisitor v1 = new EvaluatePostfixListVisitor();
+		n1.accept(v1);
+		assertEquals(18.0, v1.getResult(), DELTA);
+	}
+	
+	@Test
+	public void testComplexExpression() {
+		ListNode n1 = new NumberListNode(1.0);
+		ListNode n2 = new NumberListNode(2.0);
+		ListNode n3 = new AdditionListNode();
+		ListNode n4 = new NumberListNode(3.0);
+		ListNode n5 = new MultiplicationListNode();
+		ListNode n6 = new NumberListNode(6.0);
+		ListNode n7 = new AdditionListNode();
+		ListNode n8 = new NumberListNode(2.0);
+		ListNode n9 = new NumberListNode(3.0);
+		ListNode n10 = new AdditionListNode();
+		ListNode n11 = new DivisionListNode();
+		
+		n1.setNext(n2);
+		n2.setNext(n3);
+		n3.setNext(n4);
+		n4.setNext(n5);
+		n5.setNext(n6);
+		n6.setNext(n7);
+		n7.setNext(n8);
+		n8.setNext(n9);
+		n9.setNext(n10);
+		
+		n10.setNext(n11); //Expression is 1 2 + 3 * 6 + 2 3 + /
+		EvaluatePostfixListVisitor v1 = new EvaluatePostfixListVisitor();
+		n1.accept(v1);
+		assertEquals(3.0, v1.getResult(), DELTA);
+	}
 }
