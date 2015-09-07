6a848ca75ce71408e36bc5beef6631d19226d897
sv387 <sv387@cornell.edu>
2015-09-07 15:26:32 -0400

    Javadoc and inline comments for the prefix and postfix expression list builder classes along with their test classes

--- src/cs4321/project1/BuildPrefixExpressionTreeVisitor.java
+++ src/cs4321/project1/BuildPrefixExpressionTreeVisitor.java
@@ -4,24 +4,41 @@ import cs4321.project1.list.*;
 import cs4321.project1.tree.*;
 
 /**
- * Provide a comment about what your class does and the overall logic
- * 
+ * This class constructs Prefix list representation of the intermediate infix tree
+ * by appending the current node to the list and 
+ * traversing  recursively the left child node and the right child node
  * @author Your names and netids go here
  */
 public class BuildPrefixExpressionTreeVisitor implements TreeVisitor {
 
     private ListNode tempResult;
     private ListNode result;
+    
+    /* 
+     * Constructor for the BuildPrefixExpressionTreeVisitor 
+     */
 
     public BuildPrefixExpressionTreeVisitor() {
 		tempResult = null;
         result = null;
 	}
+    
+    /*
+	 * Method to return the resultant Prefix expression List 
+	 */
 
 	public ListNode getResult() {
 		// TODO fill me in
 		return result;
 	}
+	
+	/*
+	 * Method to read the value of leaf (number) node
+	 * and updates the temporary list that has its head node 
+	 * referenced by result list forming the prefix representation of the given expression
+	 * 
+	 * @see cs4321.project1.TreeVisitor#visit(cs4321.project1.tree.LeafTreeNode)
+	 */
 
 	@Override
 	public void visit(LeafTreeNode node) {
@@ -34,6 +51,13 @@ public class BuildPrefixExpressionTreeVisitor implements TreeVisitor {
         }
         tempResult = tempResult.getNext();
 	}
+	
+	/*
+	 * On encountering a UnaryMinus tree node appends the unary minus node 
+	 * to the expression list and recursively calls the accept method on its single child
+	 * for traversing further deep 
+	 * @see cs4321.project1.TreeVisitor#visit(cs4321.project1.tree.UnaryMinusTreeNode)
+	 */
 
 	@Override
 	public void visit(UnaryMinusTreeNode node) {
@@ -48,6 +72,14 @@ public class BuildPrefixExpressionTreeVisitor implements TreeVisitor {
 
         node.getChild().accept(this);
 	}
+	
+	/*
+	 * On encountering an Addition operator node traverses recursively in preorder ie
+	 * Appends the temp list with the addition node
+	 * left child node to construct a subset prefix expression with the leftchild node as the root
+	 * right child node to construct a subset prefix expression with the rightchild node as the root	 
+	 * @see cs4321.project1.TreeVisitor#visit(cs4321.project1.tree.AdditionTreeNode)
+	 */
 
 	@Override
 	public void visit(AdditionTreeNode node) {
@@ -63,6 +95,14 @@ public class BuildPrefixExpressionTreeVisitor implements TreeVisitor {
         node.getLeftChild().accept(this);
         node.getRightChild().accept(this);
 	}
+	
+	/*
+	 * On encountering an Multiplication operator node traverses recursively in preorder ie
+	 * Appends the temp list with the multiplication node
+	 * left child node to construct a subset prefix expression with the leftchild node as the root
+	 * right child node to construct a subset prefix expression with the rightchild node as the root	
+	 * @see cs4321.project1.TreeVisitor#visit(cs4321.project1.tree.MultiplicationTreeNode)
+	 */
 
 	@Override
 	public void visit(MultiplicationTreeNode node) {
@@ -78,6 +118,13 @@ public class BuildPrefixExpressionTreeVisitor implements TreeVisitor {
         node.getLeftChild().accept(this);
         node.getRightChild().accept(this);
 	}
+	/*
+	 * On encountering an Subtraction operator node traverses recursively in preorder ie
+	 * Appends the temp list with the subtraction node
+	 * left child node to construct a subset prefix expression with the leftchild node as the root
+	 * right child node to construct a subset prefix expression with the rightchild node as the root	
+	 * @see cs4321.project1.TreeVisitor#visit(cs4321.project1.tree.SubtractionTreeNode)
+	 */
 
 	@Override
 	public void visit(SubtractionTreeNode node) {
@@ -89,10 +136,17 @@ public class BuildPrefixExpressionTreeVisitor implements TreeVisitor {
             tempResult = new SubtractionListNode();
             result = tempResult;
         }
-
         node.getLeftChild().accept(this);
         node.getRightChild().accept(this);
 	}
+	
+	/*
+	 *On encountering an Division operator node traverses recursively in preorder ie
+	 * Appends the temp list with the division node
+	 * left child node to construct a subset prefix expression with the leftchild node as the root
+	 * right child node to construct a subset prefix expression with the rightchild node as the root	
+	 * @see cs4321.project1.TreeVisitor#visit(cs4321.project1.tree.DivisionTreeNode)
+	 */
 
 	@Override
 	public void visit(DivisionTreeNode node) {
@@ -104,7 +158,6 @@ public class BuildPrefixExpressionTreeVisitor implements TreeVisitor {
             tempResult = new DivisionListNode();
             result = tempResult;
         }
-
         node.getLeftChild().accept(this);
         node.getRightChild().accept(this);
 	}
